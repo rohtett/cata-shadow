a07ca1c01d4c24dd1f95582d80378b89042cd30c
Rohit <raheet@hotmail.co.uk>
2022-09-01 07:04:45 +0100

    page header added, initial margins adjusted

---
+++ src/Header.tsx
@@ -0,0 +1,24 @@
+const menu: string[] = ["introduction", "talents", "rotation", "stats"]
+const Header = () => {
+  return (
+    <header>
+      <div id="logo">
+        <img src="" />
+        <h2>Shadow Priest</h2>
+      </div>
+      <nav>
+        <ul>
+        { menu.map((el:string) => (
+          <a href="#">
+            <li>
+              { el }
+            </li>
+          </a>
+        ))}
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header;
