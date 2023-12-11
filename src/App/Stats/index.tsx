9cd9fac28f0893c1920e94f67afa491c9e2141ca
Rohit <raheet@hotmail.co.uk>
2023-12-11 18:33:22 +0000

    changed values of stats

--- src/App/Stats/index.tsx
+++ src/App/Stats/index.tsx
@@ -30,19 +30,19 @@ const Stats = (props:any) => {
         <br />
         Early priest playstyles would benefit greatly from as much haste as possible, however by the Firelands patch there is so much haste available that it is better to stop at specific caps. When you reach this point, it is always reccomended to aim for the +31.26% haste cap, but for your information, we have provided details of each haste cap that is available to priests.</p>
         <h4><Spell title="Devouring Plague"spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/></h4>
-        <p>At the +31.26% haste cap, Devouring Plague will have a total of three additional ticks. The second additional tick is granted at the other haste cap, +18.74%. The first additional tick is automatically gained simply from the use of Shadowform's Mind Quickening as well as Darkness talents.</p>
+        <p>At the +31.26% haste cap, <Spell title="Devouring Plague" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/> will have a total of three additional ticks. The second additional tick is granted at the second haste cap, +18.74%. The first additional tick is automatically gained simply from the use of <Spell title="Shadowform" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/>'s Mind Quickening as well as <Spell title="Darkness" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/> talents.</p>
         <span className = "tableContainer">
           <Table haste = "+18.74%" base = "1255" goblin = "1115" di = "845" goblindi = "710" />
-          <Table haste = "+31.26%" base = "2737" goblin = "2583" di = "2283" goblindi = "2135" />
+          <Table haste = "+31.26%" base = "2737" goblin = "2584" di = "2283" goblindi = "2135" />
         </span>
         <h4><Spell title="Shadow Word: Pain"spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/></h4>
-        <p>While the +24.97% haste cap, which grants a second additional tick, is often the first one Shadow Priests will aim for, the other cap cannot be attained unless the Priest is a Goblin having the Warlock buff, Dark Intent with full T-13 Best in Slot gear. This final +41.67% haste cap grants the third additional tick. Again, t first additional tick is automatically gained simply from the use of Shadowform's Mind Quickening as well as Darkness talents.</p>
+        <p>While the +24.97% haste cap, which grants a second additional tick, is often the first one Shadow Priests will aim for, the other cap cannot be attained unless the Priest is a Goblin having the Warlock buff, Dark Intent with full T-13 Best in Slot gear. This final +41.67% haste cap grants the third additional tick. Again, the first additional tick is automatically gained simply from the use of <Spell title="Shadowfiend" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/>'s Mind Quickening as well as <Spell title="Darkness" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/> talents.</p>
         <span className = "tableContainer">
           <Table haste = "+24.97%" base = "1993" goblin = "1845" di = "1562" goblindi = "1419" />
           <Table haste = "+41.67%" base = "3970" goblin = "3804" di = "3482" goblindi = "3320" />
         </span>
         <h4><Spell title="Vampiric Touch"spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/></h4>
-        <p> The +30.00% haste cap will grant the Priest a second additional tick of Vampiric Touch, with the first tick being given from the Shadowform's Mind Quickening as well as Darkness talent's passive bonuses to haste.</p>
+        <p> The +30.00% haste cap will grant the Priest a second additional tick of Vampiric Touch, with the first tick being given from the <Spell title="Shadowform" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/>'s Mind Quickening as well as <Spell title="Darkness" inline="true" spec={props.spec} viewer={props.viewer} setViewer={props.setViewer}/> talent's passive bonuses to haste.</p>
         <span className = "tableContainer">
           <Table haste = "+30.00%" base = "2589" goblin = "2437" di = "2149" goblindi = "1993" />
         </span>
